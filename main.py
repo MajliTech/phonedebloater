a930d9bb7e80db64800b4c31817de8ab72d4df11
Miłosz <hello@majlitech.tk>
2022-03-23 19:43:11 +0100

    Better way of storing packages names

--- main.py
+++ main.py
@@ -1,19 +1,58 @@
 import tkinter
 from tkinter import ttk
+import json
+from numpy import who
+import subprocess
 root = tkinter.Tk()
+
 root.title("OneUI Debloater")
 root.iconbitmap('icon.ico')
 img = tkinter.PhotoImage(file='bannerpng.png')
 tkinter.Label(
     root,
     image=img
-).grid(column=0,row=0)
-def start_uninstall(evvent=None):
+).pack() #grid(column=0,row=0)
+def real_uninstaller():
     root.destroy()
+    confirm.destroy()
+    un_win = tkinter.Tk()
+    un_win.title("Uninstalling...")
+    un_win.iconbitmap('icon.ico')
+    text=ttk.Label(un_win,text="Preparing...")
+    bar = ttk.Progressbar(un_win, orient='horizontal', length=400, mode='indeterminate')
+    bar.pack()
+    text.pack()
+    un_win.update()
+    actualUn = []
+    for i in importer:
+        if i.get() =="":
+            pass
+        else:
+           actualUn.append(i.get()) 
+        un_win.update()
+    bar["mode"]='determinate'
+    bar["maximum"]=len(actualUn)
+    un_win.update()
+    c=0
+    for i in actualUn:
+        c=c+1
+        text["text"]=f"Uninstalling {c} of {len(actualUn)} ({i})"
+        bar["value"]=c
+        un_win.update()
+        subprocess.call(f"adb shell pm uninstall -k --user 0 {i}")
+    text["text"]=f"Done!"
+def start_uninstall(evvent=None):
+    global confirm
     confirm = tkinter.Tk()
+<<<<<<< Updated upstream
+=======
+    confirm.title("U sure?")
+    confirm.iconbitmap('icon.ico')
+>>>>>>> Stashed changes
     confirm.geometry("400x200")
-    ttk.Label(confirm,text="Parsing selected into one variable...").grid(column=0,row=0)
+    ttk.Label(confirm,text="Are you sure about what you're going to do? Please make sure you\nhave alternative apps for uninstalled.").pack() #grid(column=0,row=0)
     confirm.update()
+<<<<<<< Updated upstream
     uninstall=[arzone.get(),ardrawing.get()]
 ##Uninstallers
 #Definiting what should be uninsstalled
@@ -101,4 +140,33 @@ ttk.Checkbutton(root,text="Dex On PC",variable=dexonpc,onvalue='1',offvalue='0')
 ttk.Checkbutton(root,text="Device Health Services",variable=devicehealthservices,onvalue='1',offvalue='0').grid(column=0,row=12)
 ttk.Checkbutton(root,text="Briefing",variable=briefing,onvalue='1',offvalue='0').grid(column=0,row=13)
 ttk.Button(root,text="Get rid of selected bloatware!",command=start_uninstall).grid(column=0,row=14)
+=======
+    ttk.Button(confirm, text="Yes",command=real_uninstaller).pack() #grid(row=1,column=0)
+    ttk.Button(confirm, text="No",command=confirm.destroy).pack() #grid(row=1,column=1)
+#load json and make some checkboxes with it
+import json
+applist = open("applist.json")
+whole_json=""
+for i in applist.readlines():
+    whole_json = whole_json+ i.replace("\n","")
+def parse(event=None):
+    uninstall=[]
+    for a in importer:
+        uninstall.append(a.get())
+    print(uninstall)
+"""
+ttk.Checkbutton(root,text="Bixby Pack",variable=bixbypack,onvalue='1',offvalue='0').grid(column=0,row=5)
+ttk.Label(root,text="Bixby,Bixby Vision,Bixby Voice,Bixby Vision Framework,Bixby Service").grid(column=0,row=6)
+"""
+ttk.Label(root,text="tesxt").pack() #.grid(column=0,row=16)
+importer=[]
+counter=0
+j=json.loads(whole_json)
+print()
+for i in list(j["packages"].items()):
+    importer.append(tkinter.StringVar())
+    ttk.Checkbutton(root,text=i[0],variable=importer[counter],onvalue=i[1],offvalue="",command=parse).pack()
+    counter=counter+1
+ttk.Button(root,text="Delete this bloat!",command=start_uninstall).pack()
+>>>>>>> Stashed changes
 root.mainloop()
